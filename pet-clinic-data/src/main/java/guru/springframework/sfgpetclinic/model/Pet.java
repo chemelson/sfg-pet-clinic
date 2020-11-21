e089249dd9e3c41236159ea03efa0580ae5a033c
chemelson <ross03@mail.ru>
2020-11-21 15:31:59 +0300

    Create Spring Data JPA Service for Owners. Closes #41

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -14,7 +14,7 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "type_id")
     private PetType petType;
     @ManyToOne
-    @Column(name = "owner_id")
+    @JoinColumn(name = "owner_id")
     private Owner owner;
     @Column(name = "birth_date")
     private LocalDate birthDate;
